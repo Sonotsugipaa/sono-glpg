37e647b293d2675d5873e940a8d44a7777b41db2
Parola Marco <mark.prl.97@gmail.com>
2019-02-01 10:47:57 +0100

    + Uniforms

--- shader/gltest_v.glsl
+++ shader/gltest_v.glsl
@@ -1,11 +1,13 @@
 #version 300 es
 
+uniform mat4 uni_trans;
+uniform mat4 uni_proj;
 in vec3 in_position;
 in vec4 in_color;
 
 out vec4 ex_color;
 
 void main(void) {
-    gl_Position = vec4(in_position, 1.0);
-    ex_color = in_color;
+	gl_Position = uni_proj * uni_trans * vec4(in_position, 1.0);
+	ex_color = in_color;
 }
