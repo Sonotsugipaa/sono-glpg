bdf1f3773190c30083eda8378005f16ae493c2c0
Parola Marco <mark.prl.97@gmail.com>
2019-02-23 13:33:34 +0100

    + Baker arrays / variables with spaces);
    ~ README accordingly;
    + makefile rules for itob and baker for Windows (Baker compilation is supposed to fail)

--- src/main/baker.cpp
+++ src/main/baker.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <map>
 #include <exception>
@@ -8,6 +9,7 @@ using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
+using std::stringstream;
 using std::pair;
 
 
@@ -43,7 +45,6 @@ namespace {
 			str_map map;
 			const char * arg;
 
-			map["br"] = "\n";
 			map["?"] = "?";
 			map["!"] = "!";
 
@@ -54,7 +55,37 @@ namespace {
 					const char * arg_next = *(args++);
 					if(arg_next == nullptr)
 						break;
-					map[arg+1] = arg_next;
+
+					if(arg_next[0] == '[') {
+						stringstream strs;
+
+						// remaining characters in 'arg_next'
+						strs << arg_next+1 << ' ';
+
+						while((arg_next = *(args++)) != nullptr) {
+							strs << arg_next << ' ';
+
+							strs.seekg(-2, strs.end); // -2 because the last char is a whitespace
+							if(strs.peek() == ']') {
+								/*
+								strs.seekp(-2, strs.end);
+								strs << '\0' << '\0';
+								*/
+								//cout << "\033[1;34m"<<c<<"\033[m" << endl;
+								break;
+							}
+						}
+						if(arg_next == nullptr) args -= 1;
+
+						/* this must be terribly inefficient, since two copies
+						 * of the buffer are created;
+						 * thank the STL sucking so much. */
+						string put = strs.str();
+						put.resize(put.size() - 2);
+						map[arg+1] = put;
+					} else {
+						map[arg+1] = arg_next;
+					}
 				} else
 				if(arg[0] == '?') {
 					auto found = map.find(string(arg+1));
