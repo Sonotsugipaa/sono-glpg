678456c9301feb18abc4bb44ea615da83234e070
Parola Marco <mark.prl.97@gmail.com>
2019-02-25 12:47:10 +0100

    - OpenGL features from Runtime init;
    They should be managed from the application.

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -53,14 +53,6 @@ namespace gla {
 		glewExperimental = GL_TRUE;
 		glewInit();
 
-		// GL features
-		glDepthFunc(GL_LEQUAL);
-		glEnable(GL_DEPTH_TEST);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glEnable(GL_BLEND);
-		glEnable(GL_CULL_FACE);
-		glCullFace(GL_BACK);
-
 		// GL Shader
 		shader = new ShaderProgram(vshader, fshader);
 		shader->use();
